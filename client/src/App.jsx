47764386ba9309bc9e4d9696affe7b61ba96554a
srijan300 <paulsrijan300@gmail.com>
2025-06-24 21:48:44 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const { authUser } = useContext(AuthContext)
   return (
+    //chat
     <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
